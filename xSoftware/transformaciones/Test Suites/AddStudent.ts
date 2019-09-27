1c25940d51ded3aa88067c4f9cf0369fc50a61fd
MATEO DEVIA VEGA <m.devia@sis.virtual.uniandes.edu.co>
2019-09-26 19:10:38 -0500

    all transformations done, ready for testing

---
+++ xSoftware/transformaciones/Test Suites/AddStudent.ts
@@ -0,0 +1,69 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>AddStudent</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>4d9d11ea-80d3-4013-99a5-0cea4f5c3a0f</testSuiteGuid>
+   <testCaseLink>
+      <guid>401a5200-9cdf-40b5-94f1-16f2ecd59e09</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/AddStudent</testCaseId>
+      <testDataLink>
+         <combinationType>MANY</combinationType>
+         <id>424daea7-c916-4f22-a7de-c0455e0ff432</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/student</testDataId>
+      </testDataLink>			
+	   <variables>
+	      <testDataLinkId>424daea7-c916-4f22-a7de-c0455e0ff432</testDataLinkId>
+	      <type>DATA_COLUMN</type>
+	      <value>first_name</value>
+	      <variableId>10d1a722-5272-4668-a0d7-412c9cb1201f</variableId>
+	   </variables>			
+	   <variables>
+	      <testDataLinkId>424daea7-c916-4f22-a7de-c0455e0ff432</testDataLinkId>
+	      <type>DATA_COLUMN</type>
+	      <value>last_name</value>
+	      <variableId>20d1a722-5272-4668-a0d7-412c9cb1201f</variableId>
+	   </variables>			
+	   <variables>
+	      <testDataLinkId>424daea7-c916-4f22-a7de-c0455e0ff432</testDataLinkId>
+	      <type>DATA_COLUMN</type>
+	      <value>gender</value>
+	      <variableId>30d1a722-5272-4668-a0d7-412c9cb1201f</variableId>
+	   </variables>			
+	   <variables>
+	      <testDataLinkId>424daea7-c916-4f22-a7de-c0455e0ff432</testDataLinkId>
+	      <type>DATA_COLUMN</type>
+	      <value>code</value>
+	      <variableId>40d1a722-5272-4668-a0d7-412c9cb1201f</variableId>
+	   </variables>			
+	   <variables>
+	      <testDataLinkId>424daea7-c916-4f22-a7de-c0455e0ff432</testDataLinkId>
+	      <type>DATA_COLUMN</type>
+	      <value>country</value>
+	      <variableId>50d1a722-5272-4668-a0d7-412c9cb1201f</variableId>
+	   </variables>			
+	   <variables>
+	      <testDataLinkId>424daea7-c916-4f22-a7de-c0455e0ff432</testDataLinkId>
+	      <type>DATA_COLUMN</type>
+	      <value>city</value>
+	      <variableId>60d1a722-5272-4668-a0d7-412c9cb1201f</variableId>
+	   </variables>			
+	   <variables>
+	      <testDataLinkId>424daea7-c916-4f22-a7de-c0455e0ff432</testDataLinkId>
+	      <type>DATA_COLUMN</type>
+	      <value>university</value>
+	      <variableId>70d1a722-5272-4668-a0d7-412c9cb1201f</variableId>
+	   </variables>   </testCaseLink>
+</TestSuiteEntity>
