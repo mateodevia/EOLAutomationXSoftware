9d54be27e84ffaf8f8ca9aecaff11bf6763929d4
MATEO DEVIA VEGA <m.devia@sis.virtual.uniandes.edu.co>
2019-09-28 10:01:43 -0500

    names updates to have an id on the tests and new test added

--- xSoftware/ProyectoTransformaciones/ProyectoKatalon/Scripts/DeleteStudent/Script6567746860749.groovy
+++ xSoftware/ProyectoTransformaciones/ProyectoKatalon/Scripts/DeleteStudent/Script6567746860749.groovy
@@ -11,12 +11,7 @@ import com.kms.katalon.core.testdata.TestData as TestData
 import com.kms.katalon.core.testobject.TestObject as TestObject
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import internal.GlobalVariable as GlobalVariable	
-import groovy.json.JsonSlurper
-import org.junit.After
-
-response = WS.sendRequest(findTestObject('GETCitiesByCountry', [('country') : "$country", ('state') : "$state"]))
-
-List json = new JsonSlurper().parseText(response.getResponseText())
-WS.verifyResponseStatusCode(response, 200)
-WS.verifyGreaterThanOrEqual(json.size(), 10)
+import internal.GlobalVariable as GlobalVariable
+response = WS.sendRequest(findTestObject('DeleteStudent', [('id') : "$id"]))
+WS.verifyResponseStatusCode(response, 300)
+WS.verifyElementPropertyValue(response, 'status', 'success')
