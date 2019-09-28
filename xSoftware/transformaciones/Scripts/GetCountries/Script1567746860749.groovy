6cc00debe9fffbf90907f3abbbf30420f8ff9677
MATEO DEVIA VEGA <m.devia@sis.virtual.uniandes.edu.co>
2019-09-27 22:01:42 -0500

    minor change in script

--- xSoftware/transformaciones/Scripts/GetCountries/Script1567746860749.groovy
+++ xSoftware/transformaciones/Scripts/GetCountries/Script1567746860749.groovy
@@ -13,6 +13,6 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
-response = WS.sendRequest(findTestObject('GetCountries', []))
+response = WS.sendRequest(findTestObject('GetCountries'))
 
 WS.verifyResponseStatusCode(response, 200)
